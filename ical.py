02eed48fdc0ee4000b8454939b57756d82d2c512
null <null@null>
2022-03-30 07:37:42 +0000

    added link to ICS (icalendar) file from event page

--- ical.py
+++ ical.py
@@ -1,29 +1,32 @@
 from flask import Blueprint
 from flask import request
+from flask import Response
 import settings as s
 
 ical = Blueprint("ical", __name__)
 
-@ical.route("/ical/<fn>.ics")
+@ical.route("/e/<fn>.ics")
 def show_ics(fn):
     with open(f"./data/{fn}", "r") as data:
         data = data.read().splitlines()
     with open("ics.txt", "r") as temp:
         temp = temp.read()
-    chars = {"&lt;":"<", "&gt;": ">",
+    chars = {"<br>": "\\n", ",": "\,",
+             "&lt;":"<", "&gt;": ">",
              "&apos;":"'", "&quot;": "\"",
-             "<br>": "\\n", ",": "\,"}
+             }
     e = {"fn":fn, "iso":0, "title":0,
          "host":0, "desc":0, "loc":0}
     e["iso"] = f"{fn[:8]}T{fn[8:10]}0000Z"
     e["title"] = data[0].replace(",", "\,")
     e["host"] = data[1].split(">")[0].replace(",", "\,")
-    e["desc"] = data[4]
+    e["loc"] = data[2]
+    e["desc"] = data[3]
     for c in chars:
         if c in e["desc"]:
             e["desc"] = e["desc"].replace(c, chars[c])
-    e["loc"] = data[3]
-    return eval(f'f"""{temp}"""')
+    output = eval(f'f"""{temp}"""')
+    return Response(output, mimetype="text/calendar")
 
 show_ics("2022033123.00.txt")
 show_ics("2022042018.00.txt")
